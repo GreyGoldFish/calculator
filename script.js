0a6709b0c2335a8430b38209fa80c2941d3bd1a7
Lucas Aquino de Assis <GreyGoldFish@protonmail.com>
2023-09-10 22:13:30 -0300

    Add calculator operator

---
+++ script.js
@@ -0,0 +1,41 @@
+let operator, num1, num2;
+
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            sub(num1, num2);
+            break;
+        case "*":
+            mul(num1, num2);
+            break;
+        case "/":
+            div(num1, num2);
+            break;
+        default:
+            break;
+    }
+}
+
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+
+function mul(num1, num2) {
+    return num1 * num2;
+}
+
+
+function div(num1, num2) {
+    return num1 / num2;
+}
