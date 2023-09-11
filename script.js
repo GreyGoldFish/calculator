10dba4d186179cb6c5d7236dcd7668218fc71814
Lucas Aquino de Assis <GreyGoldFish@protonmail.com>
2023-09-10 23:00:11 -0300

    Populate the display

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let operator, num1, num2;
+let displayValue = 0;
 
 window.onload = main;
 
 
 function main() {
-    
+    const displayText = document.querySelector("#display-text");
+    const digits = document.querySelectorAll(".digit");
+
+    displayText.innerHTML = "";
+    digits.forEach(digit => {
+        digit.addEventListener("click", digitClickListener);
+    });
 }
 
 
@@ -16,10 +23,10 @@ function operate(operator, num1, num2) {
         case "-":
             sub(num1, num2);
             break;
-        case "*":
+        case "×":
             mul(num1, num2);
             break;
-        case "/":
+        case "÷":
             div(num1, num2);
             break;
         default:
@@ -45,4 +52,12 @@ function mul(num1, num2) {
 
 function div(num1, num2) {
     return num1 / num2;
+}
+
+
+function digitClickListener(event) {
+    const displayText = document.querySelector("#display-text");
+
+    displayValue = parseInt(event.target.innerHTML);
+    displayText.innerHTML = displayValue;
 }
