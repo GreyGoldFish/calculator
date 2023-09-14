7e5c05097fa9b926b31083e1af6a6fa2cf03a9b5
Lucas Aquino de Assis <GreyGoldFish@protonmail.com>
2023-09-14 01:37:20 -0300

    Make calculator work properly

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+const MAX_DECIMALS = 2;
+
+const operators = {
+    '+': (num1, num2) => num1 + num2,
+    '-': (num1, num2) => num1 - num2,
+    '*': (num1, num2) => num1 * num2,
+    '/': (num1, num2) => num1 / num2,
+};
+
 let operation = []
-let displayValue = "";
 
 window.onload = main;
 
@@ -7,98 +15,69 @@ window.onload = main;
 function main() {
     const displayText = document.querySelector("#display-text");
     const digits = document.querySelectorAll(".digit");
-    const operations = document.querySelectorAll(".operation");
+    const operatorButtons = document.querySelectorAll(".operator");
 
-    displayText.innerHTML = displayValue;
+    displayText.innerHTML = "";
     digits.forEach(digit => {
         digit.addEventListener("click", digitClickListener);
     });
-    operations.forEach(operation => {
-        operation.addEventListener("click", operationClickListener);
+    operatorButtons.forEach(operatorButton => {
+        operatorButton.addEventListener("click", operatorButtonClickListener);
     })
 }
 
 
-function operate(num1, operator, num2) {
-    let result;
-
-    switch (operator) {
-        case "+":
-            result = add(num1, num2);
-            break;
-        case "-":
-            result = sub(num1, num2);
-            break;
-        case "*":
-            result = mul(num1, num2);
-            break;
-        case "/":
-            result = div(num1, num2);
-            break;
-        default:
-            break;
-    }
-
-    return result;
+function operate (num1, operator, num2) {
+    return operators[operator](num1, num2);
 }
 
 
-function add(num1, num2) {
-    return num1 + num2;
+function evaluateOperation() {
+    // Collapses the array with the result of the operation.
+    operation.splice(0, 3, operate(operation[0], operation[1], operation[2]));
+    // Ensures that return only has up to 'MAX_DECIMALS' decimals.
+    return parseFloat(operation[0].toFixed(MAX_DECIMALS));
 }
 
 
-function sub(num1, num2) {
-    return num1 - num2;
-}
-
-
-function mul(num1, num2) {
-    return num1 * num2;
-}
-
-
-function div(num1, num2) {
-    return num1 / num2;
-}
-
-
-function updateDisplay() {
+function operatorButtonClickListener(event) {
     const displayText = document.querySelector("#display-text");
-
-    displayText.innerHTML = displayValue;
-}
-
-
-function operationClickListener(event) {
     const operator = event.target.innerHTML;
 
-    operation.push(parseInt(displayValue));
+    if (operator === "AC") {
+        displayText.innerHTML = "";
+        operation = [];
+        return;
+    }
+    else if (operator === "DEL") {
+        if (operation.length === 0) {
+            displayText.innerHTML = displayText.innerHTML.slice(0, -1);
+        }
+        return;
+    }
 
-    displayValue = "";
+    const num = parseFloat(displayText.innerHTML);
+
+    operation.push(num);
     
-    // Checks for both cases of end of operation, i.e. 'num1 + num2' OR 'num1 ='.
-    if (operation.length === 3 || operator === "=") {
-        // Collapses the array with the result of the operation.
-        operation.splice(0, 3, operate(operation[0], operation[1], operation[2]));
-        displayValue = operation[0];
+    if (operation.length === 3) {
+        displayText.innerHTML = evaluateOperation();
+    }
+    else {
+        displayText.innerHTML = "";
     }
     
     operation.push(operator);
-
-    updateDisplay();
 }
 
 
 function digitClickListener(event) {
-    const digit = event.target.innerHTML
+    const displayText = document.querySelector("#display-text");
+    const digit = event.target.innerHTML;
 
-    // Checks if operation is waiting for second number and resets the display.
     if (operation.length === 2) {
-        displayValue = "";
+        displayText.innerHTML = "";
     }
 
-    displayValue += digit;
-
-    updateDisplay();
+    displayText.innerHTML += digit;
 }
